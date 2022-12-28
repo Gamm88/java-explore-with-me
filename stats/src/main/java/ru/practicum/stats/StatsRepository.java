9126e3b825580d3cf269c3ca165d3f48dbf369e8
gamm <igor_gamm@mail.ru>
2022-12-28 14:29:55 +0700

    Первая версия, корректировки с учётом требований тестов в ГИТ v4

--- stats/src/main/java/ru/practicum/stats/StatsRepository.java
+++ stats/src/main/java/ru/practicum/stats/StatsRepository.java
@@ -14,15 +14,15 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
     @Query(value = "" +
             "SELECT DISTINCT ip " +
             "FROM hits " +
-            "WHERE hit_date BETWEEN ?1 AND ?2 "
-            , nativeQuery = true)
+            "WHERE hit_date BETWEEN ?1 AND ?2 ",
+            nativeQuery = true)
     List<Hit> findAllByUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query(value = "" +
             "SELECT DISTINCT ip " +
             "FROM hits " +
-            "WHERE hit_date BETWEEN ?1 AND ?2 AND uri IN(?3)"
-            , nativeQuery = true)
+            "WHERE hit_date BETWEEN ?1 AND ?2 AND uri IN(?3)",
+            nativeQuery = true)
     List<Hit> findAllByUrisAndUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     List<Hit> findAllByHitDateBetween(LocalDateTime start, LocalDateTime end);
@@ -30,7 +30,7 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
     @Query(value = "" +
             "SELECT COUNT(ip) " +
             "FROM hits " +
-            "WHERE uri LIKE ?1"
-            , nativeQuery = true)
+            "WHERE uri LIKE ?1",
+            nativeQuery = true)
     int getViews(String uri);
 }
